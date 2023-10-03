e8e988eedeb49c77cc345d51eae02e7ba340db79
Benjamin Morant <morantbenjamin@gmail.com>
2023-10-03 17:19:56 +0200

    mocking LocalDate.now() for integration tests

--- src/main/java/com/glady/benmorant/service/UserBalanceService.java
+++ src/main/java/com/glady/benmorant/service/UserBalanceService.java
@@ -4,18 +4,29 @@ import com.glady.benmorant.exception.BeyondExpirationException;
 import com.glady.benmorant.model.Item;
 import com.glady.benmorant.model.User;
 
+import java.time.Clock;
 import java.time.LocalDate;
 
 public class UserBalanceService {
 
     private final User user;
 
+    private Clock clock;
+
     public UserBalanceService(User user) {
         this.user = user;
+        this.clock = Clock.systemDefaultZone();
+    }
+
+
+    // for integration testing purposes
+    public UserBalanceService(User user, Clock clock) {
+        this.user = user;
+        this.clock = clock;
     }
 
     public long calculateUserBalance(Item item) {
-        if (item.getExpirationDate().isAfter(LocalDate.now())) {
+        if (item.getExpirationDate().isAfter(LocalDate.now(clock))) {
             return user.getBalance() + item.getAmount();
         } else {
             throw new BeyondExpirationException("This deposit is beyond expiration. The " + item.getItemType().getName() + " amount ($" + item.getAmount() + ") hasn't been counted in " + user.getName() + "'s balance.");
--- src/test/java/com/glady/benmorant/BackendChallengeIntegrationTests.java
+++ src/test/java/com/glady/benmorant/BackendChallengeIntegrationTests.java
@@ -2,10 +2,10 @@ package com.glady.benmorant;
 
 import com.glady.benmorant.model.*;
 import com.glady.benmorant.service.DepositDistributionService;
+import com.glady.benmorant.service.UserBalanceService;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -14,23 +14,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BackendChallengeIntegrationTests {
 
     private final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/uuuu");
+
     private DepositDistributionService depositDistributionService;
+
+    private UserBalanceService userBalanceService;
 
     @Test
     void should_give_the_correct_output_for_gift_and_the_correct_user_balance_when_now_is_before_the_expiration_date() {
         // GIVEN
         String expectedOutput = "John receives a Gift distribution with the amount of $100 from Tesla. He will therefore have $100 in gift cards in his account. He received it on 06/15/2021. The gift distribution will expire on 06/14/2022.";
         User user = new User("John");
-        // Todo : new balance to calculate
-        long newBalance = 100L;
+
         LocalDate distributionDate = LocalDate.of(2021, Month.JUNE, 15);
+        Clock mockedNow = Clock.fixed(
+                Instant.parse("2021-08-22T10:00:00Z"),
+                ZoneOffset.UTC);
         Item item = new Gift(100L, distributionDate);
         item.addUsers(List.of(user));
         Company company = new Company("Tesla", 200L);
         depositDistributionService = new DepositDistributionService(company);
+        userBalanceService = new UserBalanceService(user, mockedNow);
         // WHEN
         depositDistributionService.distribute(List.of(item));
-        String actualOutput = user.getName() + " receives a " + item.getItemType().getName() + " distribution with the amount of $" + item.getAmount() + " from " + company.getName() + ". He will therefore have $" + newBalance + " in gift cards in his account. He received it on " + dtf.format(item.getDistributionDate()) + ". The gift distribution will expire on " + dtf.format(item.getExpirationDate()) + ".";
+        long userBalance = userBalanceService.calculateUserBalance(item);
+
+        String actualOutput = user.getName() + " receives a " + item.getItemType().getName() + " distribution with the amount of $" + item.getAmount() + " from " + company.getName() + ". He will therefore have $" + userBalance + " in gift cards in his account. He received it on " + dtf.format(item.getDistributionDate()) + ". The gift distribution will expire on " + dtf.format(item.getExpirationDate()) + ".";
         // THEN
         assertEquals(expectedOutput, actualOutput);
     }
@@ -40,20 +48,23 @@ class BackendChallengeIntegrationTests {
         // GIVEN
         String expectedOutput = "Jessica receives a Meal distribution from Apple with the amount of $50 on 01/01/2020, the distribution ends on 02/28/2021.";
         User user = new User("Jessica");
-        // Todo : new balance to calculate
-        long newBalance = 50L;
         long expectedBalance = 50L;
         LocalDate distributionDate = LocalDate.of(2020, Month.JANUARY, 1);
+        Clock mockedNow = Clock.fixed(
+                Instant.parse("2020-08-22T10:00:00Z"),
+                ZoneOffset.UTC);
         Item item = new Meal(50L, distributionDate);
         item.addUsers(List.of(user));
         Company company = new Company("Apple", 200L);
         depositDistributionService = new DepositDistributionService(company);
+        userBalanceService = new UserBalanceService(user, mockedNow);
         // WHEN
         depositDistributionService.distribute(List.of(item));
+        long userBalance = userBalanceService.calculateUserBalance(item);
         String actualOutput = user.getName() + " receives a " + item.getItemType().getName() + " distribution from " + company.getName() + " with the amount of $" + item.getAmount() + " on " + dtf.format(item.getDistributionDate()) + ", the distribution ends on " + dtf.format(item.getExpirationDate()) + ".";
         // THEN
         assertEquals(expectedOutput, actualOutput);
-        assertEquals(expectedBalance, newBalance);
+        assertEquals(expectedBalance, userBalance);
 
     }
 
@@ -62,21 +73,23 @@ class BackendChallengeIntegrationTests {
         // GIVEN
         String expectedOutput = "Jessica receives a Meal distribution from Apple with the amount of $50 on 01/01/2023, the distribution ends on 02/29/2024.";
         User user = new User("Jessica");
-        // Todo : new balance to calculate
-        long newBalance = 50L;
         long expectedBalance = 50L;
         LocalDate distributionDate = LocalDate.of(2023, Month.JANUARY, 1);
+        Clock mockedNow = Clock.fixed(
+                Instant.parse("2023-08-22T10:00:00Z"),
+                ZoneOffset.UTC);
         Item item = new Meal(50L, distributionDate);
         item.addUsers(List.of(user));
-
         Company company = new Company("Apple", 200L);
         depositDistributionService = new DepositDistributionService(company);
+        userBalanceService = new UserBalanceService(user, mockedNow);
         // WHEN
         depositDistributionService.distribute(List.of(item));
+        long userBalance = userBalanceService.calculateUserBalance(item);
         String actualOutput = user.getName() + " receives a " + item.getItemType().getName() + " distribution from " + company.getName() + " with the amount of $" + item.getAmount() + " on " + dtf.format(item.getDistributionDate()) + ", the distribution ends on " + dtf.format(item.getExpirationDate()) + ".";
         // THEN
         assertEquals(expectedOutput, actualOutput);
-        assertEquals(expectedBalance, newBalance);
+        assertEquals(expectedBalance, userBalance);
 
     }
 }
